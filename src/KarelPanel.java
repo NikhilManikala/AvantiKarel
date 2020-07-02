5b9c582397ddd07546de0503cf141b4fae252466
JoshanParmar <joshanparmar01@gmail.com>
2020-07-02 21:57:45 +0100

    Karel Draw Function

--- src/KarelPanel.java
+++ src/KarelPanel.java
@@ -1,5 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
+import java.util.Arrays;
 
 public class KarelPanel extends JPanel {
     int paddingX;
@@ -24,7 +25,10 @@ public class KarelPanel extends JPanel {
         super.paint(g);
         drawGrid(g);
         drawBeeper(g, 2, 3,cellWidth, cellHeight, 3);
-        drawKarel(g, 1, 4, cellWidth, cellHeight);
+        drawKarel(g, 0, 4, 0, cellWidth, cellHeight);
+        drawKarel(g, 1, 4, 1, cellWidth, cellHeight);
+        drawKarel(g, 2, 4, 2, cellWidth, cellHeight);
+        drawKarel(g, 3, 4, 3, cellWidth, cellHeight);
     }
 
     private void drawGrid(Graphics g) {
@@ -55,48 +59,60 @@ public class KarelPanel extends JPanel {
         g.drawString(Integer.toString(number), x1, y2);
     }
 
-    private void drawKarel(Graphics g, int x, int y, int cellWidth, int cellHeight){
-        int x1 = (int) ((x+0.3)* cellWidth);
-        int x2 = (int) ((x+0.8)* cellWidth);
-        int x3 = (int) ((x+0.9)* cellWidth);
-        int x4 = (int) ((x+0.9)* cellWidth);
-        int x5 = (int) ((x+0.4)* cellWidth);
-        int x6 = (int) ((x+0.3)* cellWidth);
+    private void drawKarel(Graphics g, int x, int y, int dir, int cellWidth, int cellHeight){
+        double[] xShiftList = {0.3,  0.8,  0.9,  0.9, 0.4, 0.3,  0.45, 0.75, 0.75, 0.45, 0.6, 0.75, 0.75, 0.6,  0.6, 0.7, 0.7, 0.8, 0.8, 0.6, 0,   0.3, 0.3, 0.1, 0.1, 0  };
+        double[] yShiftList = {0.01, 0.01, 0.15, 0.7, 0.7, 0.55, 0.1,  0.1,  0.5,  0.5,  0.6, 0.6,  0.62, 0.62, 0.7, 0.7, 0.9, 0.9, 1,   1,   0.4, 0.4, 0.5, 0.5, 0.6, 0.7};
 
-        int y1 = (int) ((y)*cellHeight);
-        int y2 = (int) ((y)*cellHeight);
-        int y3 = (int) ((y+0.15)*cellHeight);
-        int y4 = (int) ((y+0.7)*cellHeight);
-        int y5 = (int) ((y+0.7)*cellHeight);
-        int y6 = (int) ((y+0.55)*cellHeight);
+        for (int id = 0; id < xShiftList.length; id++) {
+            double workingX=xShiftList[id];
+            double workingY=yShiftList[id];
+            for (int i = 0; i < dir; i++) {
+                double tempX = workingX;
+                double tempY = workingY;
+                workingX = (-1)*tempY;
+                workingY = tempX;
+            }
+            if (dir == 0){
+
+            } else if (dir == 1) {
+                workingX ++;
+            } else if (dir == 2) {
+                workingX ++;
+                workingY ++;
+            } else if (dir == 3) {
+                workingY ++;
+            }
+            xShiftList[id] = workingX;
+            yShiftList[id] = workingY;
+        }
+        System.out.println(Arrays.toString(xShiftList));
+        System.out.println(Arrays.toString(yShiftList));
+
+        int[] xCoordList = new int[xShiftList.length];
+        int[] yCoordList = new int[yShiftList.length];
+
+        for (int id = 0; id < xShiftList.length; id++) {
+            xCoordList[id] = (int) ((x + xShiftList[id])*cellWidth);
+            yCoordList[id] = (int) ((y + yShiftList[id])*cellHeight);
+        }
+
+        System.out.println(Arrays.toString(xCoordList));
+        System.out.println(Arrays.toString(yCoordList));
 
         g.setColor(Color.black);
-        g.drawPolygon(new int[]{x1, x2, x3, x4, x5, x6}, new int[]{y1, y2, y3, y4, y5, y6}, 6);
+        g.drawPolygon(splice(xCoordList,0, 5), splice(yCoordList, 0, 5), 6);
+        g.drawPolygon(splice(xCoordList,6, 9), splice(yCoordList, 6, 9), 4);
+        g.fillPolygon(splice(xCoordList,10, 13), splice(yCoordList, 10, 13), 4);
+        g.fillPolygon(splice(xCoordList,14, 19), splice(yCoordList, 14, 19), 6);
+        g.fillPolygon(splice(xCoordList,20, 25), splice(yCoordList, 20, 25), 6);
+    }
 
-        int x7 = (int) ((x+0.45)*cellWidth);
-        int x8 = (int) ((x+0.75)*cellWidth);
-        int x9 = (int) ((x+0.75)*cellWidth);
-        int x10 = (int) ((x+0.45)*cellWidth);
-
-        int y7 = (int) ((y+0.1)*cellHeight);
-        int y8 = (int) ((y+0.1)*cellHeight);
-        int y9 = (int) ((y+0.5)*cellHeight);
-        int y10 = (int) ((y+0.5)*cellHeight);
-
-        g.drawPolygon(new int[]{x7, x8, x9, x10}, new int[]{y7, y8, y9, y10}, 4);
-
-        int x11 = (int) ((x+0.6)*cellWidth);
-        int x12 = (int) ((x+0.7)*cellWidth);
-        int x13 = (int) ((x+0.7)*cellWidth);
-        int x14 = (int) ((x+0.6)*cellWidth);
-
-        int y11 = (int) ((y+0.6)*cellHeight);
-        int y12 = (int) ((y+0.6)*cellHeight);
-        int y13 = (int) ((y+0.62)*cellHeight);
-        int y14 = (int) ((y+0.62)*cellHeight);
-
-        g.fillPolygon(new int[]{x11, x12, x13, x14}, new int[]{y11, y12, y13, y14}, 4);
-
-//        int x15 = (int) ((x+))
+    private static int[] splice(int[] list, int start, int end){
+        int[] toReturn = new int[end-start+1];
+        for (int i = 0; i < (end-start+1); i++) {
+            toReturn[i]=list[i+start];
+        }
+        System.out.println(Arrays.toString(toReturn));
+        return toReturn;
     }
 }
