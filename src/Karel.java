c7fc024c1e1f7b9858701e7fa0bfc419d451e413
JoshanParmar <joshanparmar01@gmail.com>
2020-07-02 20:14:05 +0100

    Wrote rendering engine for Karel Grid

--- src/Karel.java
+++ src/Karel.java
@@ -1,38 +1,104 @@
+import java.awt.*;
 import javax.swing.*;
-import java.util.Arrays;
-import java.util.Random;
-
 public class Karel {
-    private static int rows=5;
-    private static int columns=5;
-    private static int[][] Beepers = new int[rows][columns];
+//    Graphical Constants
+    private static final int paddingX = 40;
+    private static final int paddingY = 40;
 
-    public static void main(String[] args) {
-        System.out.println(Arrays.deepToString(Beepers));
+    private static final int cellWidth = 100;
+    private static final int cellHeight = 100;
 
-        JFrame frame = new JFrame("Demo");
-        frame.setSize(550, 250);
-        frame.setVisible(true);
 
-        int delay = 500;
-        Timer timer = new Timer( delay, e -> {
-            Render renderState = new Render();
-            renderState.setBeepers(Beepers);
-            frame.add(renderState);
-            frame.setVisible(true);
-            System.out.println("Hello World!");
-            addRandomBeeper();
-        }
-        );
+//    Technical Constants
+    private static final int rows = 5;
+    private static final int columns = 5;
 
-        timer.start();
 
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//    Calculated Constants
+    private static final int panelWidth = columns*cellWidth;
+    private static final int panelHeight = rows*cellHeight;
+
+    private static final int windowWidth = panelWidth + 10*paddingX;
+    private static final int windowHeigth = panelHeight + 3*paddingY;
+
+    public Karel() {
+        setupJFrame();
     }
 
-    private static void addRandomBeeper() {
-        int rnd1 = new Random().nextInt(Beepers.length);
-        int rnd2 = new Random().nextInt(Beepers[rnd1].length);
-        Beepers[rnd1][rnd2]++;
+    private void setupJFrame() {
+
+        JFrame f= new JFrame("Karel R");
+        KarelPanel panel= new KarelPanel(paddingX, paddingY, cellWidth, cellHeight, rows, columns);
+
+        f.setBackground(Color.LIGHT_GRAY);
+
+        panel.setBounds(paddingX, paddingY, panelWidth, panelHeight);
+        panel.setBackground(Color.WHITE);
+
+        f.add(panel);
+        f.setSize(windowWidth, windowHeigth);
+
+
+        f.setLayout(null);
+        f.setVisible(true);
+    }
+
+    public static void main(String args[])
+    {
+        new Karel();
+    }
+}
+
+class KarelPanel extends JPanel{
+    int paddingX;
+    int paddingY;
+    int cellWidth;
+    int cellHeight;
+    int rows;
+    int columns;
+
+    public KarelPanel(int paraPaddingX, int paraPaddingY, int paraCellWidth, int paraCellHeight, int paraRows, int paraCols){
+        paddingX = paraPaddingX;
+        paddingY = paraPaddingY;
+        cellWidth = paraCellWidth;
+        cellHeight = paraCellHeight;
+        rows = paraRows;
+        columns = paraCols;
+
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+        drawGrid(g);
+        drawBeeper(g, 2, 3,cellWidth, cellHeight, 3);
+    }
+
+    private void drawGrid(Graphics g) {
+        int rad = 2;
+        for (int row = 0; row < rows; row++) {
+            int centerX = (int) ((row+0.5)*cellWidth);
+            for (int col = 0; col < columns; col++) {
+                int centerY = (int) ((col+0.5)*cellHeight);
+                g.fillOval(centerX-rad, centerY-rad, 2*rad, 2*rad);
+            }
+        }
+    }
+
+    private void drawBeeper(Graphics g, int x, int y, int cellWidth, int cellHeight, int number){
+        int x1 = (int) ((x + 0.5) * cellWidth);
+        int x2 = ((x + 1) * cellWidth);
+        int x3 = (int) ((x + 0.5) * cellWidth);
+        int x4 = ((x) * cellWidth);
+        int y1 = ((y) * cellHeight);
+        int y2 = (int) ((y + 0.5) * cellHeight);
+        int y3 = (int) ((y + 1) * cellHeight);
+        int y4 = (int) ((y + 0.5) * cellHeight);
+
+        g.setColor(Color.lightGray);
+        g.fillPolygon(new int[]{x1, x2, x3, x4}, new int[]{y1, y2, y3, y4}, 4);
+
+        g.setColor(Color.black);
+        g.drawString(Integer.toString(number), x1, y2);
     }
 }
